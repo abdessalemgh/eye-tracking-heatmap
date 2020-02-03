7951385de8fbae354308a53d79497de92179ede2
abdessalemgh <ghozia.abdessalem@gmail.com>
2020-02-03 17:17:03 +0100

    add show user

--- pages/includes/user_show.php
+++ pages/includes/user_show.php
@@ -1,7 +1,7 @@
 <div class="col-12 mt-5">
     <div class="card">
         <div class="card-body">
-            <h4 class="header-title"><?php echo $user->nom . ' ' . $user->prenom . " (" .$user->age ." ans)"?></h4>
+            <h4 class="header-title"><?php echo $user->nom . ' ' . $user->prenom . " (" . $user->age . " ans)" ?></h4>
             <div class="data-tables">
                 <table id="usersTable" class="text-center">
                     <thead class="bg-light text-capitalize">
@@ -14,16 +14,16 @@
                     </thead>
                     <tbody>
                     <?php
-                    foreach($user->getTests() as $testedImage){
+                    foreach ($user->getTests() as $testedImage) {
                         $row = "<tr>";
                         $row .= "<td> pas encore resigned</td>";
-                        $row .= "<td  ". (false? "class='badge badge-success'> passed" : "class='badge badge-danger'> Non passé") ."</td>";
-                        $row .= "<td> ". $testedImage['imagePath'] ."</td>";
-                        $row .= "<td> ". (true? "<a href='#'> <i class='ti-eye' title='faire le test'></i></a>": "") ."</td>";
+                        $row .= "<td  " . (false ? "class='badge badge-success'> passed" : "class='badge badge-danger'> Non passé") . "</td>";
+                        $row .= "<td> " . $testedImage['imagePath'] . "</td>";
+                        $row .= "<td> " . (true ? "<a href='#'> <i class='ti-eye' title='faire le test'></i></a>" : "") . "</td>";
                         $row .= "</tr>";
                         echo $row;
                     }
-                        ?>
+                    ?>
                     </tbody>
                 </table>
             </div>
